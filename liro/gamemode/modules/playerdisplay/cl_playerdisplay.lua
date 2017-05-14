510d5ad24590ac91db07cc999396c3b089f1199d
Alydus <officialalydus@gmail.com>
2017-05-14 01:37:49 +0100

    Removed example modules

--- liro/gamemode/modules/playerdisplay/cl_playerdisplay.lua
+++
@@ -1,6 +0,0 @@
-// Player Display Module
-// Example Module for Liro
-
-hook.Add("HUDPaint", "PlayerDisplay", function()
-	draw.SimpleText(#player.GetAll() .. "/" .. game.MaxPlayers(), "Default", 50, 50, Color(255,255,255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
-end)
