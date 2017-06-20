303b83568aecfbce3da706d08ce3d51636c86086
Alydus <business@alydus.net>
2017-06-20 12:51:36 +0100

    Add example blacklistedFiles to registermodule.lua

--- liro/gamemode/modules/examplemodule/registermodule.lua
+++ liro/gamemode/modules/examplemodule/registermodule.lua
@@ -5,7 +5,7 @@
 -- Must be set (case sensitive)
 -- If 'folderName' is not set, Liro will completely fail to load!
 local folderName = "examplemodule"
-local moduleData = '{"folderName": "' .. folderName .. '", "loadPriority": 5,"author": "Alydus","description": "A example module","website": "alydus.net","version": "0.1", "networkStrings": ["networkString1", "networkString2"], "loadPrefixes": {"server": "sv_", "client": "cl_", "shared": "sh_"}}'
+local moduleData = '{"folderName": "' .. folderName .. '", "loadPriority": 5,"author": "Alydus","description": "A example module","website": "alydus.net","version": "0.1", "blacklistedFiles": ["sv_testfile.lua"], "networkStrings": ["networkString1", "networkString2"], "loadPrefixes": {"server": "sv_", "client": "cl_", "shared": "sh_"}}'
 
 -- Register the module - Do not touch
 hook.Run("liro.registerModule", moduleData)
