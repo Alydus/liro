7d5829b22134f2f51b8b92d8230f88eebfc68a23
Alydus <officialalydus@gmail.com>
2019-03-14 09:53:29 +0000

    Update shared.lua for V2 preparation

--- liro/gamemode/shared.lua
+++ liro/gamemode/shared.lua
@@ -1,8 +1,13 @@
 -- Liro - shared.lua
 
 -- Declare gamemode information
+
+-- Change these to your gamemodes information, feel free to remove credit for Liro if you wish.
+-- GM.Name will display as the gamemode name in the new server browser.
+-- The foldername of your gamemode will determine which server gamemode category it is in, common ones are ttt, darkrp, starwarsrp.
+
 GM.Name = "Liro"
-GM.Author = "Various Contributors"
+GM.Author = "Alydus"
 GM.Email = ""
 GM.Website = "https://github.com/Alydus/liro"
-GM.Version = "1.9"
+GM.Version = "2.0"
