b41af366932c40f94f3117db821504c09ee8631d
Alydus <officialalydus@gmail.com>
2019-05-02 10:39:42 +0100

    Update shared.lua with GM.Version change warning

--- liro/gamemode/shared.lua
+++ liro/gamemode/shared.lua
@@ -4,6 +4,8 @@
 
 -- Change these to your gamemodes information, feel free to remove credit for Liro if you wish.
 -- GM.Name will display as the gamemode name in the new server browser.
+-- GM.Version should not be changed, as this is used as an internal versioning system for Liro and it's version checker.
+-- If you want to have a version for your gamemode, please include it somewhere else, or create a different version variable (e.g. GM.ContentVersion)
 -- The foldername of your gamemode will determine which server gamemode category it is in, common ones are ttt, darkrp or starwarsrp.
 
 GM.Name = "Liro"
