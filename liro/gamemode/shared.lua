5e5d248ca0674da23abc1ba963d9a38d0de56105
Alydus <officialalydus@gmail.com>
2019-05-03 09:48:57 +0100

    Create new GM.LiroVersion for update checking

--- liro/gamemode/shared.lua
+++ liro/gamemode/shared.lua
@@ -4,12 +4,13 @@
 
 -- Change these to your gamemodes information, feel free to remove credit for Liro if you wish.
 -- GM.Name will display as the gamemode name in the new server browser.
--- GM.Version should not be changed, as this is used as an internal versioning system for Liro and it's version checker.
--- If you want to have a version for your gamemode, please include it somewhere else, or create a different version variable (e.g. GM.ContentVersion)
+-- Do not edit GM.LiroVersion, this is used for Liro version checking and verification of the base.
+-- It is recommended to make use of Semantic Versioning with GM.Version, this will help you distribute your Liro gamemode better. (semver.org/)
 -- The foldername of your gamemode will determine which server gamemode category it is in, common ones are ttt, darkrp or starwarsrp.
 
 GM.Name = "Liro"
 GM.Author = "Alydus"
 GM.Email = ""
 GM.Website = "https://github.com/Alydus/liro"
-GM.Version = "2.0"
+GM.Version = "0.0.1"
+GM.LiroVersion = "2.0"
