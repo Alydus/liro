fd9a47a09c049a8446823803975a1bd25457daae
Alydus <officialalydus@gmail.com>
2019-05-24 23:48:21 +0100

    Patch gamemode references in init.lua

--- liro/gamemode/init.lua
+++ liro/gamemode/init.lua
@@ -47,9 +47,9 @@ include("liro/moduleloader.lua")
 AddCSLuaFile("liro/moduleloader.lua")
 
 if isRefresh and liro.bootComplete then
-	liro.diagnosticPrint("Lua Auto-Refresh loaded Liro " .. GAMEMODE.LiroVersion .. " serverside code in " .. math.Round(os.clock() - liro.startTime, 3) .. " second(s).")
+	liro.diagnosticPrint("Lua Auto-Refresh loaded Liro " .. GM.LiroVersion .. " serverside code in " .. math.Round(os.clock() - liro.startTime, 3) .. " second(s).")
 else
-	liro.diagnosticPrint("Successfully loaded Liro " .. GAMEMODE.LiroVersion .. " in " .. math.Round(os.clock() - liro.startTime, 3) .. " second(s).")
+	liro.diagnosticPrint("Successfully loaded Liro " .. GM.LiroVersion .. " in " .. math.Round(os.clock() - liro.startTime, 3) .. " second(s).")
 end
 
 liro.bootComplete = true
